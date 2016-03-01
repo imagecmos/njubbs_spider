2fd622eb691fc8c0d1c194f9d554f9114a98f70b
imagecmos <hy_caott@163.com>
2016-03-01 15:26:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import bbs_top10
 import time
 import hotword
-<<<<<<< HEAD
 import logging
 
 
@@ -12,15 +11,4 @@ while True:
     print( ''' wait for next crawler and sleep for 1800s
 ##############################################################################
     ''')
-=======
-import subprocess
-
-while True:
-    subprocess.Popen('cmd python hotword.py',shell=True)
-    cre=bbs_top10.crawler()
-    cre.run()
-    print ''' wait for next crawler and sleep for 1800s
-##############################################################################
-    '''
->>>>>>> 91ac9e3ebc8f7d34b1106636e14cae53662cf618
     time.sleep(1800)
